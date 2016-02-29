a7a808630151f7d25df222f506e11e222b5ed6ef
Buddy Magsipoc <keikun17@gmail.com>
2016-02-29 16:16:06 +0800

    Comment scaffold。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+  root 'pages#index'
+
   resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
